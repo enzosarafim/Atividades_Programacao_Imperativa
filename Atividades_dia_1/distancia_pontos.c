6aa624903b550969ec68b199a8daeb5b95e9f624
Enzo Nogueira Sarafim <enzosarafim@gmail.com>
2026-03-06 16:35:23 -0300

    Correct input handling for point coordinates
    Fix scanf to use address of variables for xp2 and yp2.

--- Atividades_dia_1/distancia_pontos.c
+++ Atividades_dia_1/distancia_pontos.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(){
+
+    double xp1;
+    double yp1;
+    double xp2;
+    double yp2;
+    double calculo;
+
+    scanf("%lf %lf", &xp1, &yp1);
+    scanf("%lf %lf", xp2, yp2);
+
+    calculo = sqrt(pow(xp2 - xp1, 2.0) + pow(yp2 - yp1, 2.0));
+    printf("%.4lf\n", calculo);
+
+    return 0;
+}
