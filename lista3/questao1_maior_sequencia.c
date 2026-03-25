b05ff8b391ee54d251aeab89a12fabdf16858bb7
Enzo Nogueira Sarafim <enzosarafim@gmail.com>
2026-03-25 11:05:07 -0300

    Update questao1_maior_sequencia.c

--- lista3/questao1_maior_sequencia.c
+++ lista3/questao1_maior_sequencia.c
@@ -3,53 +3,47 @@
 int main() {
     int T;
     scanf("%d", &T);
-    
-    
-    //T-- = while(T > 0){... t--}
-    while (T--) {
+
+    for (int caso = 0; caso < T; caso++) {
         int N;
         scanf("%d", &N);
 
-        int seq[N];
-        for (int i = 0; i < N; i++)
-            scanf("%d", &seq[i]);
+        int v[100];
 
-        // Variáveis para rastrear a melhor sequência encontrada
-        int melhor_inicio = 0, melhor_tamanho = 1;
-        // Variáveis para rastrear a sequência atual
-        int atual_inicio = 0, atual_tamanho = 1;
+        for (int i = 0; i < N; i++) {
+            scanf("%d", &v[i]);
+        }
+
+        int inicio_atual = 0;
+        int tamanho_atual = 1;
+
+        int melhor_inicio = 0;
+        int melhor_tamanho = 1;
 
         for (int i = 1; i < N; i++) {
-            if (seq[i] < seq[i - 1]) {
-                // Continua a sequência decrescente
-                atual_tamanho++;
+            if (v[i] < v[i - 1]) {
+                tamanho_atual++;
             } else {
-                // Sequência quebrou, verifica se é a melhor até agora
-                if (atual_tamanho > melhor_tamanho) {
-                    melhor_tamanho = atual_tamanho;
-                    melhor_inicio = atual_inicio;
-                }
-                // Reinicia sequência atual
-                atual_inicio = i;
-                atual_tamanho = 1;
+                inicio_atual = i;
+                tamanho_atual = 1;
+            }
+
+            if (tamanho_atual > melhor_tamanho) {
+                melhor_tamanho = tamanho_atual;
+                melhor_inicio = inicio_atual;
             }
         }
 
-        // Verifica a última sequência (se não cair no else anterior)
-        if (atual_tamanho > melhor_tamanho) {
-            melhor_tamanho = atual_tamanho;
-            melhor_inicio = atual_inicio;
-        }
-
-        //resultado
-        
-        if (melhor_tamanho > 1) {
+        if (melhor_tamanho < 2) {
+            printf("0\n");
+        } else {
             printf("%d\n", melhor_tamanho);
-            for (int i = melhor_inicio; i < melhor_inicio + melhor_tamanho; i++)
-                printf("%d ", seq[i]);
+
+            for (int i = melhor_inicio; i < melhor_inicio + melhor_tamanho; i++) {
+                printf("%d ", v[i]);
+            }
+
             printf("\n");
-        }else {
-            printf("0");
         }
     }
 
