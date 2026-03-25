9ffa32fc48a065625e757637d026d56c683282fc
Enzo Nogueira Sarafim <enzosarafim@gmail.com>
2026-03-25 10:34:17 -0300

    Refactor client handling and update logic

--- lista3/questao2_coragem.c
+++ lista3/questao2_coragem.c
@@ -5,70 +5,70 @@ int main() {
     scanf("%d %d %d", &total_clientes, &multiplo, &clientes_sistema);
 
     int latas_por_cliente[1001] = {0};
-    int horario_termino[1001] = {0};
+    int horario_por_cliente[1001] = {0};
 
-    if (multiplo != 0) {
-        for (int i = 1; i <= total_clientes; i++) {
-            if (i % multiplo == 0) {
-                latas_por_cliente[i] = 1;
-            }
-        }
-    }
-
+    // L� o sistema primeiro
     for (int i = 0; i < clientes_sistema; i++) {
-        int quantidade_latas;
         char horario[6];
+        int quantidade_latas;
 
         scanf("%s %d", horario, &quantidade_latas);
 
         int hora   = (horario[0] - '0') * 10 + (horario[1] - '0');
         int minuto = (horario[3] - '0') * 10 + (horario[4] - '0');
 
-        int minutos_desde_abertura = (hora * 60 + minuto) - (7 * 60);
-        int numero_cliente = minutos_desde_abertura / 3;
+        // Hor�rio de t�rmino: 07:00 + cliente*3 -> cliente = (min - 420) / 3
+        int total_minutos = hora * 60 + minuto;
+        int numero_cliente = (total_minutos - 420) / 3;
 
-        latas_por_cliente[numero_cliente] = quantidade_latas;
-        horario_termino[numero_cliente]   = hora * 60 + minuto;
+        latas_por_cliente[numero_cliente]  = quantidade_latas;
+        horario_por_cliente[numero_cliente] = total_minutos;
     }
 
-    int total_latas_vendidas = 0;
-    int cliente_vencedor     = -1;
-    int latas_do_vencedor    = 0;
-    int horario_do_vencedor  = 0;
-
-    for (int i = 1; i <= total_clientes; i++) {
-        if (latas_por_cliente[i] > 0) {
-            total_latas_vendidas += latas_por_cliente[i];
-
-            if (cliente_vencedor == -1 && total_latas_vendidas >= 50) {
-                cliente_vencedor    = i;
-                latas_do_vencedor   = latas_por_cliente[i];
-
-                if (horario_termino[i] != 0)
-                    horario_do_vencedor = horario_termino[i];
-                else
-                    horario_do_vencedor = 7 * 60 + i * 3;
+    // D� 1 lata aos m�ltiplos de X que N�O est�o no sistema
+    if (multiplo != 0) {
+        for (int i = 1; i <= total_clientes; i++) {
+            if (i % multiplo == 0 && latas_por_cliente[i] == 0) {
+                latas_por_cliente[i] = 1;
+                horario_por_cliente[i] = 420 + i * 3;
             }
         }
     }
 
-    if (cliente_vencedor == -1) {
-        int latas_faltando = 50 - total_latas_vendidas;
+    // Acumula latas e acha o vencedor
+    int total_latas  = 0;
+    int vencedor     = -1;
+    int latas_ganhou = 0;
+    int horario_ganhou = 0;
 
-        if (latas_faltando == 1)
-            printf("Ainda nao foram vendidas latas suficientes. Falta %d lata.\n", latas_faltando);
+    for (int i = 1; i <= total_clientes; i++) {
+        if (latas_por_cliente[i] > 0) {
+            total_latas += latas_por_cliente[i];
+
+            if (vencedor == -1 && total_latas >= 50) {
+                vencedor     = i;
+                latas_ganhou = latas_por_cliente[i];
+                horario_ganhou = horario_por_cliente[i];
+            }
+        }
+    }
+
+    if (vencedor == -1) {
+        int faltam = 50 - total_latas;
+        if (faltam == 1)
+            printf("Ainda nao foram vendidas latas suficientes. Falta %d lata.\n", faltam);
         else
-            printf("Ainda nao foram vendidas latas suficientes. Faltam %d latas.\n", latas_faltando);
+            printf("Ainda nao foram vendidas latas suficientes. Faltam %d latas.\n", faltam);
     } else {
-        int hora_atendimento   = horario_do_vencedor / 60;
-        int minuto_atendimento = horario_do_vencedor % 60;
+        int hora_saida   = horario_ganhou / 60;
+        int minuto_saida = horario_ganhou % 60;
 
-        if (latas_do_vencedor == 1)
+        if (latas_ganhou == 1)
             printf("Quem levou a cesta basica foi o %d* cliente atendido por coragem, as %02d:%02d. Que comprou %d lata.\n",
-                   cliente_vencedor, hora_atendimento, minuto_atendimento, latas_do_vencedor);
+                   vencedor, hora_saida, minuto_saida, latas_ganhou);
         else
             printf("Quem levou a cesta basica foi o %d* cliente atendido por coragem, as %02d:%02d. Que comprou %d latas.\n",
-                   cliente_vencedor, hora_atendimento, minuto_atendimento, latas_do_vencedor);
+                   vencedor, hora_saida, minuto_saida, latas_ganhou);
     }
 
     return 0;
